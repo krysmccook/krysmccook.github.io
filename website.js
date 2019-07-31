2f2fb93f71779c64cd82ecdf7b5f5afb48a0823b
Krystal <krystal.c.mccook@gmail.com>
2019-07-31 13:56:44 -0400

    Initial Commit 7/31/19

---
+++ website.js
@@ -0,0 +1,10 @@
+
+
+function cool() {
+document.getElementById('bio').innerHTML = "About Krystal McCook";
+document.getElementById('image').style.height="100px";
+}
+
+function annoy_people() {
+  alert("I'm annoying you");
+}
